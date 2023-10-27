71bbf2edcf49760d9cfe6512d6073ba7c5ae2034
thisSELFmySELF <cliffdove@gmail.com>
2023-10-27 06:12:02 -0400

    COMMIT MESSAGE

--- service/routes.py
+++ service/routes.py
@@ -9,11 +9,13 @@ from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
+
 # Health Endpoint
 @app.route("/health")
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
+
 
 # GET INDEX
 @app.route("/")
@@ -26,6 +28,7 @@ def index():
         ),
         status.HTTP_200_OK,
     )
+
 
 # CREATE A NEW ACCOUNT
 @app.route("/accounts", methods=["POST"])
@@ -45,6 +48,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 # LIST ALL ACCOUNTS
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
@@ -58,6 +62,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 # READ AN ACCOUNT
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -70,6 +75,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 # UPDATE AN EXISTING ACCOUNT
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
@@ -86,6 +92,7 @@ def update_accounts(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
+
 # DELETE AN ACCOUNT
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -98,6 +105,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 # UTILITY FUNCTIONS
 def check_content_type(media_type):
